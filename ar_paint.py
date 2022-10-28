6969fbaf52979fcfa19843209732d6a3523f5c56
Isabel <isabel.rosario@ua.pt>
2022-10-28 12:21:30 +0100

    fixed command line arguments bug and commented the code :)

--- ar_paint.py
+++ ar_paint.py
@@ -1,28 +1,48 @@
 import cv2
-import argparse
-from os import path
-import json
 import numpy as np
+
+import argparse
+import json
+
+from os import path
+import sys
 from datetime import datetime
 
+from color_segmenter import apply_mask
 
-def apply_mask(image, ranges):
-    lows = (ranges['B']['min'], ranges['G']['min'], ranges['R']['min'])
-    highs = (ranges['B']['max'], ranges['G']['max'], ranges['R']['max'])
-    return cv2.inRange(image, lows, highs)
 
 def get_centroid(mask):
+    """
+    function 'get_centroid': analyses the result of a mask being applied to an image (the result being
+                            a binary image) in order to find its largest color segment, as well as its
+                            centroid 
+        INPUT:
+            - mask: an image where a color segmentation mask has been applied; it is a BINARY
+                    image where the white pixels represent that a given color has been detected
+                    for that pixel in the original image, whereas black pixels signify the
+                    absense of that same color
+        OUTPUT:
+            - (cX, cY): the X and Y coordinates (respectively) of the centroid of the biggest
+                        object (white blob) in the 'mask' input
+            - final_image: NOT a binary image; this is an RGB image with all the detected objects in the
+                        'mask' input; the biggest object is colored green, whereas all the other ones
+                        remain white; a red cross is placed in the position of the centroid
+    """
 
     # find all contours (objects)
     cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
+    # if we detect objects, let's find the biggest one, make it green and calculate the centroid
     if cnts:
-        cnt = max(cnts, key=cv2.contourArea) # biggest object
 
+        # find the biggest object
+        cnt = max(cnts, key=cv2.contourArea)
+
+        # make it green (but still show other objects in white)
         biggest_obj = np.zeros(mask.shape, np.uint8)
         cv2.drawContours(biggest_obj, [cnt], -1, 255, cv2.FILLED)
         biggest_obj = cv2.bitwise_and(mask, biggest_obj) # mask-like image with only the biggest object
-        all_other_objs = cv2.bitwise_xor(mask, biggest_obj)
+        all_other_objs = cv2.bitwise_xor(mask, biggest_obj) # all other objects except the biggest one
         
         b = all_other_objs
         g = mask
@@ -30,26 +50,48 @@ def get_centroid(mask):
 
         final_image = cv2.merge((b, g, r))
 
-        # calculate centroid
+        # calculate centroid coordinates
         M = cv2.moments(cnt)
         cX = int(M["m10"] / M["m00"]) if (M["m00"]!=0) else None
         cY = int(M["m01"] / M["m00"]) if (M["m00"]!=0) else None
 
-        # draw the centroid
+        # draw small red cross to indicate the centroid point
         if cX and cY:
             cv2.line(final_image, (cX-8, cY-8), (cX+8, cY+8), (0, 0, 255), 5)
             cv2.line(final_image, (cX+8, cY-8), (cX-8, cY+8), (0, 0, 255), 5)
 
-    # if no detected objects, show black canvas
+    # if we don't detect any objects, we just show the mask as it is
     else:
-        # TODO: yuck
         final_image = cv2.merge((mask, mask, mask))
         cX = None
         cY = None
     
     return (cX,cY), final_image
 
+
 def draw(image, old_coords, coords, color, thickness):
+    """
+    function draw: draws on the canvas
+        INPUT:
+            - image:      this is our canvas, meaning it is the image we want to draw on
+            - old_coords: last position of the pencil
+            - coords:     new position of the pencil
+            - color:      pencil color
+            - thickness:  pencil thickness
+        OUTPUT:
+            - [return value]: there are three distinct cases for what the return value could
+                            be, altough in all of them we return an RGB image
+                            (1) if the new pencil coordinates are (None,None), then we do
+                                not draw on the image, simply return it as it was given
+                            (2) if the new pencil coordinates are DIFFERENT from (None, None)
+                                but we do not have any previous coordinates (meaning that
+                                old_coords is (None,None)), we simply draw a dot on the image
+                                and return it
+                            (3) if we have both the previous coordinates and the new ones, we
+                                draw a line going from the previous to the new coordinates on
+                                the image and return it
+    """
+
     if (not coords[0]) or (not coords[1]):
         return image
     else:
@@ -60,13 +102,20 @@ def draw(image, old_coords, coords, color, thickness):
 
 
 def main():
-    
+    """
+    function main: initializes all the necessary elements of the program and performs the
+                continuous color detection and drawing operations, as well as the key
+                detection aspect of the program
+    """
+
     # ------------ Initialization ------------
 
     # processing command line arguments
     parser = argparse.ArgumentParser(description='PSR AR Paint')
-    parser.add_argument('-j', '--json', action='store_true', help='Use this argument to provide the path to the .json file with the color data.')
+    parser.add_argument('-j', '--json', type=str, required=False, help='Use this argument to provide the path to the .json file with the color data.')
     args = vars(parser.parse_args())
+    # if a path to a .json file is not provided, we assume it's the
+    # limits.json file resulting from the execution of color_segmenter.py
     json_path = 'limits.json' if not args['json'] else args['json']
 
     # reading color information from .json file
@@ -74,8 +123,9 @@ def main():
         with open(json_path, 'r') as openfile:
             json_object = json.load(openfile)
             ranges = json_object['limits']
-    except:
-        raise ValueError('The .json file with the color data doesn\'t exist.')
+    # if the file doesn't exist, send out an error message and quit
+    except FileNotFoundError:
+        sys.exit('The .json file with the color data doesn\'t exist.')
 
 
     # setting up the video capture
@@ -111,7 +161,8 @@ def main():
 
     # ------------ Continuous Operation ------------
 
-    draw_color = (0,0,255) # default is red
+    # default pencil setup
+    draw_color = (0,0,255) # starts off red
     draw_thickness = 5
     old_centroid_coords = (None,None)
 
@@ -119,14 +170,17 @@ def main():
 
         # capture an image with the camera
         _, frame = capture.read()
-        mask = apply_mask(frame, ranges) # 2-dimensional
+        mask = apply_mask(frame, ranges)
 
+        # show camera and canvas
         cv2.imshow(camera_window, frame)
         cv2.imshow(canvas_window, canvas)
 
+        # calculate centroid of the largest color blob and show the mask being applied
         centroid_coords, detected_centroid = get_centroid(mask)
         cv2.imshow(mask_window, detected_centroid)
 
+        # update the canvas and the pencil coordinates according to the most recent drawing movement
         canvas = draw(canvas, old_centroid_coords, centroid_coords, draw_color, draw_thickness)
         old_centroid_coords = centroid_coords
 
--- color_segmenter.py
+++ color_segmenter.py
@@ -9,43 +9,75 @@ TODO:
 """
 
 def update_range_dict(val,ranges,color,bound):
+    """
+    function update_range_dict: updates the dictionary that holds the valid ranges of each
+                                of the 3 color channels for color segmentation with new
+                                trackbar values
+        INPUT:
+            - val: new trackbar value, resulting from end-user's manipulation of the
+                interface's trackbars
+            - ranges: the dictionary holding the valid RBG ranges
+            - color: the color channel that needs to be altered (so, either 'R', 'G' or 'B')
+            - bound: the bound that needs to be altered (so, either 'min' or 'max')
+    """
     ranges[color][bound] = val
 
-# Alter image according to the RBG parameters
-def alter_image(ranges, image, window_name):
+def apply_mask(image, ranges):
+    """
+    function apply_mask: applies a color segmentation mask to an image
+        INPUT:
+            - image: original image we want to apply the mask to
+            - ranges: this is basically the mask itself, except instead of being a binary image with which
+                    we would perform a logical operation to the original image, it is a dictionary
+                    indicating the valid ranges of values for each of the color channels - R, G and B
+        OUTPUT:
+            - [return value]: a binary image where the white pixels represent pixels in the original image
+                            that stood within the valid ranges for all three color channels, whereas black
+                            pixels represent pixels in the original image where at least one of the three
+                            ranges was violated; this tells us which pixels in the original image were within
+                            the valid color range we wish to detect
+    """
 
     lows = (ranges['B']['min'], ranges['G']['min'], ranges['R']['min'])
     highs = (ranges['B']['max'], ranges['G']['max'], ranges['R']['max'])
-
-    final_image = cv2.inRange(image, lows, highs)
-    cv2.imshow(window_name, final_image)
+    return cv2.inRange(image, lows, highs)
 
 
 def main():
+    """
+    function main: initializes all the necessary elements of the program and performs the continuous color
+                segmentation operation, as well as the key detection aspect of the program
+    """
 
-    # default values to start with
+    # get the color range values to start with
+    # if we already have a 'limits.json' file in the directory, we get the previously saved values from there
     fileAlreadyExists = path.exists('limits.json')
     if fileAlreadyExists:
         with open('limits.json', 'r') as openfile:
             json_object = json.load(openfile)
             ranges = json_object['limits']
+    # if not, we start with default values
     else:
         ranges = { 'B':{'max': 255, 'min': 0}, 'G':{'max': 255, 'min': 0}, 'R':{'max': 255, 'min': 0} }
 
+    # set up video capture
     capture = cv2.VideoCapture(0)
     _, frame = capture.read()
 
+    # figure out window size from the size of the frames captured by the camera
     scale = 0.55
     window_width = int(frame.shape[1] * scale)
     window_height = int(frame.shape[0])
 
+    # finish up the video capture setup
     window_name = 'Color segmentation'
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, window_width, window_height)
 
+    # max value for all the trackbars
     slider_max = 255
 
-    # Trackbars for R, G and B dimensions
+    # trackbars for R, G and B dimensions
 
     cv2.createTrackbar('R min', window_name , ranges['R']['min'], slider_max, partial(update_range_dict, ranges=ranges, color='R', bound='min'))
     cv2.createTrackbar('R max', window_name , ranges['R']['max'], slider_max, partial(update_range_dict, ranges=ranges, color='R', bound='max'))
@@ -56,11 +88,15 @@ def main():
     cv2.createTrackbar('B min', window_name , ranges['B']['min'], slider_max, partial(update_range_dict, ranges=ranges, color='B', bound='min'))
     cv2.createTrackbar('B max', window_name , ranges['B']['max'], slider_max, partial(update_range_dict, ranges=ranges, color='B', bound='max'))
 
+    # color segmentation continuous operation
     while True:
 
         # Capture frame-by-frame and display
         _, frame = capture.read()
-        alter_image(ranges,frame,window_name)
+
+        # apply color segmentation mask to the recently captured frame 
+        mask = apply_mask(frame,ranges)
+        cv2.imshow(window_name, mask)
 
         # wait for a command
         pressedKey = cv2.waitKey(1) & 0xFF
